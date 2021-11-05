357f2994c434b25659619f3697b26b22f7c46105
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2021-11-05 23:07:32 +0200

    fixes

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,8 +7,8 @@ namespace TestProject1 {
         [TestMethod]
         public void TestMethod1() {
             int[] a = {1,2,3,4,5};
-            int max = Program.MaxOddElement(a, a.Length);
-            Assert.AreEqual(5, max);
+            int index = Program.MaxOddElement(a);
+            Assert.AreEqual(4, index);
         }
     }
 }
--- lab6.2(iter)/Program.cs
+++ lab6.2(iter)/Program.cs
@@ -10,48 +10,53 @@ namespace ProgramNS {
     public class Program {
         static void Main(string[] args) {
             int []a;
-            int n, max;
-            
+            int n, index = -1;
+
             Console.Write("Enter n: ");
             n = Convert.ToInt32(Console.ReadLine());
 
             a = new int[n];
-            ArrayBuilder(a, n);
+            ArrayBuilder(a);
             Console.Write("\nArray:");
-            PrintArray(a, n);
+            PrintArray(a);
 
-            max = MaxOddElement(a, n);
-            if(max != 0)
-                Console.WriteLine($"\nMax odd element: {max}\n");
+            index = MaxOddElement(a);
+            if (index != -1)
+                Console.WriteLine($"\nMax odd element: {a[index]}\n");
             else
                 Console.WriteLine("\nThere is no odd element in array.\n");
         }
-        static int[] ArrayBuilder(int []a, int size) {
+        static int[] ArrayBuilder(int []a) {
             var rand = new Random();
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < a.Length; i++) {
                 a[i] = rand.Next(-1000, 1000);
             }
 
             return a;
         }
-        public static int MaxOddElement(int []a, int size) {
-            int max = 0;
-            for (int i = 0; i < size; i++) {
+        public static int MaxOddElement(int []a) {
+            int index = -1,
+                max = 0;
+            for (int i = 0; i < a.Length; i++) {
                 if (a[i] % 2 == 1) {
                     max = a[i];
+                    index = i;
                     break;
                 }
             }
 
-            if (max != 0) {
-                for (int i = 0; i < size; i++) {
-                    if (a[i] % 2 == 1 && a[i] > max) max = a[i];
+            if (index != -1) {
+                for (int i = 0; i < a.Length; i++) {
+                    if (a[i] % 2 != 0 && a[i] > max) {
+                        max = a[i];
+                        index = i;
+                    }
                 }
             }
 
-            return max;
+            return index;
         }
-        static void PrintArray(int []a, int n) {
+        static void PrintArray(int []a) {
             foreach (int el in a) {
                 Console.Write($"{el} ");
             }
